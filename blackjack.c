f824282f4fd69dd0ecef771baa8ae4565e6610c8
RFoss5 <riley.foss@trojans.dsu.edu>
2024-11-13 11:26:46 -0600

    Add files via upload

---
+++ blackjack.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int i=1,jqk=1;
+	int deck[52],hand[5],dealer[2];
+	while(i<40){
+	for(int m=1;m<11;m++){
+		deck[i]=m;
+		i++;
+		}
+	}
+	while(i<53){
+			deck[i]=10;
+			i++;
+	}
+	printf("\n");
+}
