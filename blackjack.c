3b96d28a7eeb948b99d5b798e788e28874fc8ed1
RFoss5 <riley.foss@trojans.dsu.edu>
2024-11-14 11:11:55 -0600

    Add files via upload

--- blackjack.c
+++ blackjack.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(){
+	srand(time(NULL));
 	int i=1,jqk=1;
 	int deck[52],hand[5],dealer[2];
 	while(i<40){
@@ -14,5 +16,12 @@ int main(){
 			deck[i]=10;
 			i++;
 	}
+	for (int l = 0; l < 2; l++) {
+  		int num = rand() % 52 + 1;
+  		hand[l]=deck[num];
+  		printf("%d ",hand[l]);
+	}
+	printf("This is your hand, what would you like to do?\n1. Hit\n2. Stay\n");
+	scanf("%d ", &jqk);
 	printf("\n");
 }
